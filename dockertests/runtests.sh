e5b573d804420684d4c765d75509e00208e3e5da
M. Peter <mpneuried@googlemail.com>
2018-11-19 21:32:13 +0100

    added docker tests

---
+++ dockertests/runtests.sh
@@ -0,0 +1,31 @@
+VERSIONS[1]="argon"
+VERSIONS[2]="boron"
+VERSIONS[3]="carbon"
+VERSIONS[4]="latest"
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+SCRIPTDIR="dockertests"
+cd $DIR
+rm -f *.log
+
+cd ..
+
+#grunt build
+
+for version in "${VERSIONS[@]}"
+do
+   :
+   FV=`echo $version | sed 's/\./_/g'`
+   DFile="Dockerfile_$FV"
+   if [ -f "$SCRIPTDIR/$DFile" ]; then
+	   DIMG=`head -n 1 $SCRIPTDIR/$DFile`
+	   echo "\n-------------\nDocker Test ($DIMG)\n-------------"
+	   BUILDLOGS="$DIR/dockerbuild_$version.log"
+	   docker build -t=mpneuried.nodecache.test:$version -f=$SCRIPTDIR/$DFile . > $BUILDLOGS
+	   docker run --rm mpneuried.nodecache.test:$version >&2
+   else
+	   echo "Dockerfile '$DFile' not found"
+   fi
+done
